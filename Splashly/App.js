39ab7d6925b46e1fbc10894f4c5913387dee3c77
Rahul Sarma <rahulsarma7007@gmail.com>
2024-03-06 21:39:35 +0530

    implementation of splash screen

--- Splashly/App.js
+++ Splashly/App.js
@@ -10,9 +10,14 @@
    Text,
    View,
  } from 'react-native';
+ import SplashScreen from 'react-native-splash-screen'
  
  const App = () => {
- 
+
+  useEffect(()=>{
+    SplashScreen.hide();
+  }, [])
+  
    return (
      <View>
        <Text>Hello World!</Text>
