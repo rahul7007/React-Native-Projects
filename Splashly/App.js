c41bb17d0b3fc76d4b3a2cc801e98fc18d1ec5b3
Rahul Sarma <rahulsarma7007@gmail.com>
2024-03-06 20:02:34 +0530

    hello world

---
+++ Splashly/App.js
@@ -0,0 +1,25 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+ import React, { useEffect } from 'react';
+ import {
+   Text,
+   View,
+ } from 'react-native';
+ 
+ const App = () => {
+ 
+   return (
+     <View>
+       <Text>Hello World!</Text>
+     </View>
+   );
+ }
+ 
+ export default App;    
+ 
+ 
